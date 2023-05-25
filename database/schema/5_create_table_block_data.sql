8a007b572c5d3c64414092c68a66d23330d7c92b
Andy Nogueira <me@andynogueira.dev>
2023-05-25 11:19:01 -0400

    remove private key (no need) from block_data table (#4)

--- database/schema/5_create_table_block_data.sql
+++ database/schema/5_create_table_block_data.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS comet.block_data
 (
     height bigint NOT NULL,
     transaction bytea NOT NULL,
-    CONSTRAINT block_height_pkey PRIMARY KEY (height),
     CONSTRAINT block_height_fk FOREIGN KEY (height)
         REFERENCES comet.result_block (block_header_height) MATCH SIMPLE
         ON UPDATE CASCADE
