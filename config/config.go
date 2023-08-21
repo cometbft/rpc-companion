e2b6ed95f3d2747070238b918e2d9ced20228cba
Andy Nogueira <me@andynogueira.dev>
2023-08-21 18:33:59 -0400

    initial boilerplate for config (#15)

--- config/config.go
+++ config/config.go
@@ -1,9 +1,27 @@
 package config
 
 type Config struct {
+	// Options for services
+	Storage *StorageConfig `mapstructure:"storage"`
 }
 
 // DefaultConfig returns a default configuration for a node
 func DefaultConfig() *Config {
 	return &Config{}
+}
+
+// DefaultStorageConfig returns a default configuration for the Storage layer
+func DefaultStorageConfig() *StorageConfig {
+	return &StorageConfig{
+		Connection: "", //TODO: Add postgres connection
+	}
+}
+
+//-----------------------------------------------------------------------------
+// StorageConfig
+
+// StorageConfig defines the configuration options for the storage layer
+type StorageConfig struct { //nolint: maligned
+	// Connection credentials
+	Connection string `mapstructure:"connection"`
 }
